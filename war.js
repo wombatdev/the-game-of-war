b4603ce6745ffb7d5295fc0808d07980d2e57996
James <james@team-wombat.com>
2016-06-03 13:59:10 -0400

    Added card images, added shuffle/deal to title click, added player/computer top card into active slot

--- war.js
+++ war.js
@@ -2,6 +2,10 @@
 var deckOfCards = ['H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8', 'H9', 'H10', 'H11', 'H12', 'H13', 'H14', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'C11', 'C12', 'C13', 'C14', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'D10', 'D11', 'D12', 'D13', 'D14', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'S10', 'S11', 'S12', 'S13', 'S14'];
 var playerDeck = [];
 var computerDeck = [];
+var turn = 1;
+
+
+$(document).on("ready", function() {
 
 function shuffle(array) {
     var m = array.length, t, i;
@@ -21,7 +25,7 @@ function shuffle(array) {
 }
 
 function deal(array) {
-    for (i = 0; i < array.length-1; i++ ) {
+    for (i = 0; i < array.length; i++ ) {
         if ((i+2)%2 == 0) {
             playerDeck.push(array[i]);
         }
@@ -31,110 +35,23 @@ function deal(array) {
     }
 }
 
+$(".title").on("click", function() {
+    shuffle(deckOfCards);
+    deal(deckOfCards);
+    console.log(playerDeck);
+    console.log(playerDeck.length);
+    console.log(computerDeck);
+    console.log(computerDeck.length);
+});
 
-// $(document).on("ready", function() {
-
-
-// function shuffle (array) {
-//   var i = 0
-//     , j = 0
-//     , temp = null
-//
-//   for (i = array.length - 1; i > 0; i -= 1) {
-//     j = Math.floor(Math.random() * (i + 1))
-//     temp = array[i]
-//     array[i] = array[j]
-//     array[j] = temp
-//   }
-// }
-
-// function Card (rank, suit) {
-//     this.rank = rank;
-//     this.suit = suit;
-// };
-// function Deck() {
-//     this.deck = new Array();
-//     this.makeDeck = makeDeck;
-//     this.shuffle = shuffle;
-//     this.deal = deal;
-// };
-// function makeDeck() {
-//     var ranks = new Array("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K");
-//     var suits = new Array("Clubs", "Diamonds", "Hearts", "Spades");
-//     var deckOfCards = [];
-//     var i, j;
-//     for (i = 0; i < suits.length; i++) {
-//         for (j = 0; j < ranks.length; j++) {
-//             deckOfCards[i] = new Card(ranks[j], suits[i])
-//             console.log("Card made \n");
-//         }
-//     }
-// };
-// function shuffleTheDeck() {
-// debugger
-//     Card();
-// debugger
-//     Deck();
-// debugger
-//     makeDeck();
-// debugger
-// }
-//     function shuffle() {
-//         var i, n, j, temp;
-//         for (i = 0; i < n; i++) {
-//             for (j = 0; j < this.deck.length; j++) {
-//                 k = Math.floor(Math.random() * this.deck.length);
-//                 temp = this.deck[j];
-//                 this.deck[j] = this.deck[k];
-//                 this.deck[k] = temp;
-//             }
-//         }
-//         document.write("Cards Shuffled");
-//     };
-//
-//
-//
-//     function deal() {
-//
-//         if (this.deck.length > 0) {
-//             return this.deck.shift();
-//         }
-//         else return null;
-//     };
-//
-//     var deck = new Deck();
-//
-//     deck.makeDeck();
-//     deck.shuffle();
-//     for (i = 0; i < 2; i++) {
-//         for (j = 0; j < 5; j++) {
-//             var Card = new Card(deck.deal);
-//             var c = JSON.stringify(Card);
-//             document.write(this.deck[j]);
-//         }
-//     }
-//
-//
-// };
+$(".click img").on("click", function() {
+    console.log("active card clicked");
+    $("#player").find(".cardstack.active").html("<img src='cards/"+playerDeck[turn-1]+".png' width=100% />");
+    $("#computer").find(".cardstack.active").html("<img src='cards/"+computerDeck[turn-1]+".png' width=100% />");
+    console.log(turn);
+    turn++;
+});
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// DOC ON READY END TAG
-// });
+//DOC ON READY END TAG
+});
