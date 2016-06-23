ee293b9962faccd66f49ff422cd497f0442b56f7
James <james@team-wombat.com>
2016-06-23 16:13:12 -0400

    updating winning transitions for mobile

--- war.js
+++ war.js
@@ -255,7 +255,7 @@ function endOfTurn () {
             else {
                 $(".real").css("transform", "translate(-1000px,-1000px)").css("transition","all 0.25s ease-in-out");
             }
-        };
+        }
         else {
             if (winner == 0) {
                 $(".real").css("transform", "translate(0px,-1000px)").css("transition","all 0.25s ease-in-out");
